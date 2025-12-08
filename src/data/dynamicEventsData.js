7c7d6c481f51fffc1cbbc4016b73164e8988327b
xivamjha <jhashivam1383@gmail.com>
2025-12-08 22:52:35 +0530

    time

--- src/data/dynamicEventsData.js
+++ src/data/dynamicEventsData.js
@@ -17,7 +17,7 @@ export const movieEventMappings = [
     id: 2,
     title: "Open Air Screening: Ghosted",
     type: "Open Air",
-    time: "8:00 PM",
+    time: "11:00 AM",
     location: "JLN gate 14, Delhi",
     description: "Join us for an emotional rollercoaster with Your Fault. Bring your blankets and experience this powerful story unfold under the open sky.",
     image: "https://sunsetcinemaclub.in/img/admin/page/home/AK9pBWeu1Y__DSC6756 (1).jpg",
